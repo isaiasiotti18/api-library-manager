bdd0902d20bb3d98ae4c41b66bedb5d9a1a3dbbc
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-02-21 13:33:01 -0300

    genero

--- src/modules/genero/genero.controller.ts
+++ src/modules/genero/genero.controller.ts
@@ -7,11 +7,13 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CriarGeneroDTO } from './dtos/criar-genero.dto';
 import { GeneroService } from './genero.service';
 import { Genero } from './model/genero.model';
 
 @Controller('/api/v1/generos')
+@ApiTags('generos')
 export class GeneroController {
   constructor(private readonly generosService: GeneroService) {}
 
