d12d01acbec9938394dd62005e4a62bef7cf7538
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-02-24 16:33:41 -0300

    Endereco

--- src/modules/endereco/dtos/criar-endereco.dto.ts
+++ src/modules/endereco/dtos/criar-endereco.dto.ts
@@ -1,36 +1,10 @@
 import { ListaUfs } from './lista-ufs.enum';
-import { IsCEP } from 'brazilian-class-validator';
-import { IsNotEmpty, IsEnum, IsString, IsNumber } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 
-export class CriarEnderecoDTO {
-  @IsNotEmpty()
-  @IsCEP()
-  @ApiProperty()
-  cep: string;
-
-  @IsNotEmpty()
-  @IsString()
-  @ApiProperty()
+export interface CriarEnderecoDTO {
+  readonly cep: string;
   readonly logradouro: string;
-
-  @IsNotEmpty()
-  @IsNumber()
-  @ApiProperty()
-  numero: number;
-
-  @IsNotEmpty()
-  @IsString()
-  @ApiProperty()
+  readonly numero: number;
   readonly bairro: string;
-
-  @IsNotEmpty()
-  @IsString()
-  @ApiProperty()
   readonly cidade: string;
-
-  @IsNotEmpty()
-  @IsEnum(ListaUfs, { message: 'UF inválido.' })
-  @ApiProperty()
   readonly uf: ListaUfs;
 }
