a86a2294192ad07e06ba9bf9c164e4ac7f24fefa
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-03-09 16:50:55 -0300

    Aws S3 Config

--- src/modules/aws/aws-s3.service.ts
+++ src/modules/aws/aws-s3.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
 import { ConfigService } from '@nestjs/config';
 
