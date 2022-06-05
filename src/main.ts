f7a5bc8b5c85db1dd6a5173d11f5724767053c7a
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-06-05 17:09:26 -0300

    security api

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-//import 'reflect-metadata';
+import 'reflect-metadata';
 
 import { AllExceptionFilter } from 'src/utils/filters/http-exception.filter';
 
@@ -7,6 +7,8 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as momentTimezone from 'moment-timezone';
+import * as compression from 'compression';
+import helmet from 'helmet';
 
 const config = new DocumentBuilder()
   .setTitle('API Library Manager')
@@ -26,6 +28,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: true,
   });
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: 'http://localhost:8000',
+    methods: ['get', 'post', 'put', 'delete', 'patch'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  app.use(compression());
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
